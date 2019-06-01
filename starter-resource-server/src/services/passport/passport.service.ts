2e5f88066b4fd4527640ab8869160a4da1b2d58a
unknown <sbenning@student.42.fr>
2019-06-01 12:01:14 +0200

    by modules

--- starter-resource-server/src/services/passport/passport.service.ts
+++ starter-resource-server/src/services/passport/passport.service.ts
@@ -3,15 +3,15 @@ import passportLocal from 'passport-local';
 import passportJWT from 'passport-jwt';
 import JWT from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
-import { LocalUserModel, LocalUser } from './local-user';
-import mongoose from 'mongoose';
+import mongoose, { Model, Document } from 'mongoose';
+import { ObjectID } from 'mongodb';
 
 export interface AuthPayload {
     username: string;
     password: string;
 }
-export interface AuthResponse {
-    user: LocalUser;
+export interface AuthResponse<User extends { _id: string | ObjectID }> {
+    user: User;
     token: string;
 }
 
@@ -27,17 +27,17 @@ export const AuthSchema = new mongoose.Schema({
 });
 export const AuthModel = mongoose.model('Auth', AuthSchema);
 
-const {
-    JWT_SECRET,
-} = process.env;
-
-export class Auth {
+export class PassportService<User extends { _id: string | ObjectID }> {
 
     passport = passport;
     passportLocal = passportLocal;
     passportJWT = passportJWT;
     
-    constructor() {
+    constructor(
+        private secret: string,
+        private userModel: Model<Document & User>,
+        private fieldName: string,
+    ) {
         this.setupLocalStrategy();
         this.setupJWTStrategy();
     }
@@ -45,7 +45,7 @@ export class Auth {
     private setupLocalStrategy() {
         passport.use(new passportLocal.Strategy(
             async function(username, password, done) {
-                const user = await LocalUserModel.findOne({ username: username });
+                const user = await this.userModel.findOne({ [this.fieldName]: username });
                 if (!user || (user.get('password') !== password)) {
                     return done(null, false);
                 } else {
@@ -58,10 +58,10 @@ export class Auth {
     private setupJWTStrategy() {
         const JWTOptions = {
             jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
-            secretOrKey: JWT_SECRET,
+            secretOrKey: this.secret,
         };
         passport.use(new passportJWT.Strategy(JWTOptions, async ({ id }, done) => {
-            const user = await LocalUserModel.findById(id);
+            const user = await this.userModel.findById(id);
             if (!user) {
                 return done(null, false);
             } else {
@@ -72,12 +72,12 @@ export class Auth {
 
     localSignupMiddleware() {
         return async (req: Request, res: Response, next: NextFunction) => {
-            const { username, password } = req.body;
-            const user = await LocalUserModel.findOne({ username });
+            const { [this.fieldName]: username, password } = req.body;
+            const user = await this.userModel.findOne({ [this.fieldName]: username });
             if (user) {
                 res.status(401).json({ message: `Username already in use.` });
             } else {
-                const data = new LocalUserModel({ username, password });
+                const data = new this.userModel({ [this.fieldName]: username, password });
                 if (!await data.save()) {
                     res.status(500).json({ message: `Something went wrong.` });
                 } else {
@@ -107,8 +107,16 @@ export class Auth {
     localSigninController() {
         return async (req: Request, res: Response) => {
             const user = req.user;
-            const token = JWT.sign({ user, id: user._id }, JWT_SECRET);
+            const token = JWT.sign({ user, id: user._id }, this.secret);
             res.json({ user, token });
         };
     }
+}
+
+export function createMainPassportService<User extends { _id: string | ObjectID }>(
+    secret: string,
+    userModel: Model<Document & User>,
+    fieldName: string = 'username',
+) {
+    return new PassportService<User>(secret, userModel, fieldName);
 }
